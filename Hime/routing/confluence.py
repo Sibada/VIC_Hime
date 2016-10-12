a911db1c58b105d424a7016faa37da9ab768a570
Sibada <zrd2017@163.com>
2016-10-12 15:28:53 +0800

    Fixes and now can make routing.

--- Hime/routing/confluence.py
+++ Hime/routing/confluence.py
@@ -25,11 +25,14 @@ def load_rout_data(file_path):
     return rout_data
 
 
-def write_rout_data(rout_data, file_path):
+def write_rout_data(rout_data, out_file):
     rout_json = demjson.encode(rout_data)
-    df = open(file_path, "w")
+    df = open(out_file, "w")
     df.writelines(rout_json)
+
+    df.writelines(["\n", "\n"])  # Write a new empty line for reading by R.
     df.close()
+    print "Rout data has been write to file %s" % out_file
 
 
 ########################################################################################################################
@@ -40,8 +43,8 @@ def write_rout_data(rout_data, file_path):
 #
 ########################################################################################################################
 def convolution(uh_cell, runoff_yield):
-
-    pre = np.dot(uh_cell, runoff_yield)
+    print "Making convolution..."
+    pre = np.dot(runoff_yield, uh_cell)
     nrow = pre.shape[0]
 
     for c in range(1, pre.shape[1]):
@@ -58,28 +61,41 @@ def convolution(uh_cell, runoff_yield):
 #
 ########################################################################################################################
 def confluence(vic_out_file, rout_data, domain_file, start_date, end_date):
-    sn = rout_data["sn"]
+    basin = rout_data["basin"]
     uh_cell = rout_data["uh_cell"]
 
+    print "Read data from %s" % vic_out_file
     vf = nc.Dataset(vic_out_file, "r")
     # TODO: Detect if it have variable names time, OUT_BASEFLOW and OUT_RUNOFF.
 
-    time_units = vf.variables["time"].units
+    nctime = vf.variables["time"]
     calendar = vf.variables["time"].calendar
     ts = pd.date_range(start_date, end_date)
-    t = nc.date2index(ts, time_units, calendar)
+    ts_dt = [ti.to_datetime() for ti in ts]
+    t = nc.date2index(ts_dt, nctime, calendar)
 
-    baseflow = vf.variables["OUT_BASEFLOW"][t, sn]
-    runoff = vf.variables["OUT_RUNOFF"][t, sn]
+    baseflow_t = vf.variables["OUT_BASEFLOW"][t, :]
+    runoff_t = vf.variables["OUT_RUNOFF"][t, :]
     vf.close()
+
+    baseflow = np.zeros((len(t), len(basin)))
+    runoff = np.zeros((len(t), len(basin)))
+    for c in range(len(basin)):
+        baseflow[:, c] = baseflow_t[:, basin[c, 1], basin[c, 0]]
+        runoff[:, c] = runoff_t[:, basin[c, 1], basin[c, 0]]
 
     df = nc.Dataset(domain_file, "r")
     # TODO: Detect if it have variable names area.
-    area = df.variables["area"][sn]
+    area = np.zeros(len(basin))
+    for c in range(len(basin)):
+        area[c] = df.variables["area"][basin[c, 1], basin[c, 0]]
 
-    runoff_yield = (runoff + baseflow) * area / 2073600000.0
+    # Runoff yield of mm should transfer to meters and divided by num of seconds in a day to transfer to m^3/s.
+    runoff_yield = (runoff + baseflow) * area / 86400000.0
 
-    daily_runoff = convolution(uh_cell, runoff_yield)
+    # Convolution.
+    daily_runoff = pd.Series(convolution(uh_cell, runoff_yield), index=ts)
+
     return daily_runoff
 
 
@@ -88,8 +104,17 @@ def confluence(vic_out_file, rout_data, domain_file, start_date, end_date):
 # Transfer runoff series from daily series to monthly series.
 #
 ########################################################################################################################
-def gather_to_month(daily_runoff, start_date, end_date):
-    ts = pd.date_range(start_date, end_date)
-    d_runoff = pd.Series(daily_runoff, index=ts)
-    monthly_runoff = d_runoff.resample("M", how="mean")
+def gather_to_month(daily_runoff):
+    monthly_runoff = daily_runoff.resample("M").mean()
     return monthly_runoff
+
+
+########################################################################################################################
+#
+# Out put runoff data.
+#
+########################################################################################################################
+def write_runoff_data(runoff_data, out_file):
+    runoff_data.to_csv(out_file, sep="\t")
+    print "File %s has been write." % out_file
+
